d81fa04d175fe38caf677f3f129b567ea29e3b23
Esrom Waktola <esrom.waktola@exactuals.com>
2020-01-09 17:35:58 -0500

    final service commits

--- src/main/java/com/cognizant/warmup/enums/Level.java
+++ src/main/java/com/cognizant/warmup/enums/Level.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Level {
 
-	ONE, TWO, THREE;
+	ONE, TWO, THREE, FOUR;
 
 	Level() {
 	}
--- src/main/java/com/cognizant/warmup/repository/EBSRepository.java
+++ src/main/java/com/cognizant/warmup/repository/EBSRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.cognizant.warmup.domain.EBS;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "ebses", collectionResourceRel = "ebses")
 public interface EBSRepository extends PagingAndSortingRepository<EBS, Integer> {
 
 }
