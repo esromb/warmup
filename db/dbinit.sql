4c71f46700c4d43d2584cc8995d3011570997fda
Esrom Waktola <esrom.waktola@exactuals.com>
2020-01-09 11:14:49 -0500

    Added rest repositories

--- db/dbinit.sql
+++ db/dbinit.sql
@@ -9,12 +9,12 @@ create table IF NOT EXISTS users
 );
 
 create table if not exists skills(
-skill_id int(11) not null primary key,
+skill_id int(11) auto_increment not null primary key,
 name varchar(20) not null
 );
 
 create table if not exists role (
-role_id int(11) not null primary key,
+role_id int(11) auto_increment not null primary key,
 skill_id int(11) not null,
 level int(1) not null,
 geo varchar(10) not null,
@@ -26,7 +26,7 @@ FOREIGN KEY (skill_id) REFERENCES skills(skill_id)
 );
 
 create table if not exists team_member (
-member_id int(11) not null primary key,
+member_id int(11) auto_increment not null primary key,
 first_name varchar(25) not null,
 last_name varchar(25) not null,
 skill_id int(11) not null,
@@ -45,7 +45,7 @@ FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
 
 create table if not exists esa (
-esa_id int(11) not null primary key,
+esa_id int(11) auto_increment not null primary key,
 member_id int(11) not null,
 esa_status varchar(20) not null,
 allocation_date date,
@@ -54,7 +54,7 @@ FOREIGN KEY (member_id) REFERENCES team_member(member_id)
 );
 
 create table if not exists ebs (
-ebs_id int(11) not null primary key,
+ebs_id int(11) auto_increment not null primary key,
 member_id int(11) not null,
 submitted_date date,
 start_date date,
