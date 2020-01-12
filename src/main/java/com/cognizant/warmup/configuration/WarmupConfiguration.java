3350d4aeb5709e2225f8ecdb88d8b3c442fd6ce1
Esrom Waktola <esrom.waktola@exactuals.com>
2020-01-11 20:08:28 -0500

    fix cors issue

--- src/main/java/com/cognizant/warmup/configuration/WarmupConfiguration.java
+++ src/main/java/com/cognizant/warmup/configuration/WarmupConfiguration.java
@@ -1,14 +1,22 @@
 package com.cognizant.warmup.configuration;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class WarmupConfiguration extends WebMvcConfigurerAdapter {
+public class WarmupConfiguration {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
-	}
+	@Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
